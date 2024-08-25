c55963c1aac62e809976ae4b95b9e4b84d12643a
Pedro Jorge <pedrojorge2011@gmail.com>
2024-08-25 16:19:11 -0300

    feat: initial colors setup with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--blue)',
+        basetitle: 'var(--base-title)',
+        basesubtitle: 'var(--base-subtitle)',
+        basetext: 'var(--base-text)',
+        basespan: 'var(--base-span)',
+        baselabel: 'var(--base-label)',
+        baseborder: 'var(--base-border)',
+        basepost: 'var(--base-post)',
+        baseprofile: 'var(--base-profile)',
+        basebackground: 'var(--base-background)',
+        baseinput: 'var(--base-input)'
+      }
+    },
+  },
+  plugins: [],
+}
+
